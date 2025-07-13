fb7677e625a7436a763309e2eab2b594b248291f
Lu <luciaalvareznu@gmail.com>
2025-07-13 23:49:55 +0200

    fix: repository name

--- src/settings.ts
+++ src/settings.ts
@@ -32,7 +32,7 @@ export const template = {
 	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
-	base: 'cv' // Repository name starting with /
+	base: '/cv' // Repository name starting with /
 }
 
 export const seo = {
