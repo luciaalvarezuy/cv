78a364a836f3c616f3b6737a2f11887fde8e6dd0
Lu <luciaalvareznu@gmail.com>
2025-07-13 23:48:39 +0200

    chore: add template options

--- src/settings.ts
+++ src/settings.ts
@@ -25,14 +25,14 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://luciaalvarezuy.github.io/cv/', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
 	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
 	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
-    base: '' // Repository name starting with /
+	base: 'cv' // Repository name starting with /
 }
 
 export const seo = {
